5024690897090eca5ba8f9ffe98bc0be9b456ae6
ta.to <61908291+tato-gh@users.noreply.github.com>
2023-01-26 20:31:34 +0900

    add RootScreen: expoのサンプルアプリケーション参考にして追加
    - 合わせて、src/App.tsxを参照するための設定抜けがあったため修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
+import { registerRootComponent } from "expo";
 import type { FC } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
+
 const App: FC = () => {
-  return(
-    <SafeAreaProvider>
-      <StatusBar />
-    </SafeAreaProvider>
-  )
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return(
+      <SafeAreaProvider>
+        <Navigation colorScheme={colorScheme} />
+        <StatusBar />
+      </SafeAreaProvider>
+    )
+  }
 };
 
+registerRootComponent(App);
 export default App;
---
+++ src/navigation/index.tsx
@@ -0,0 +1,38 @@
+/**
+ * If you are not familiar with React Navigation, refer to the "Fundamentals" guide:
+ * https://reactnavigation.org/docs/getting-started
+ *
+ */
+
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ColorSchemeName } from 'react-native';
+
+export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
+  return (
+    <NavigationContainer
+      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <RootNavigator />
+    </NavigationContainer>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+import { View, Text } from 'react-native';
+
+function RootScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function RootNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Root" component={RootScreen} />
+    </Stack.Navigator>
+  );
+}
