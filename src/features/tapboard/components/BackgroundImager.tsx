a720363bae2022e1d617be71244402bf849d9b8a
tato <61908291+tato-gh@users.noreply.github.com>
2023-02-02 16:02:32 +0900

    add 背景画像の永続化処理を追加

--- src/features/tapboard/components/BackgroundImager.tsx
+++ src/features/tapboard/components/BackgroundImager.tsx
@@ -1,11 +1,17 @@
 import type { FC, PropsWithChildren } from 'react';
-import { useState } from 'react';
+import { useState, useLayoutEffect } from 'react';
 import * as ImagePicker from 'expo-image-picker';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import BackgroundImage from './BackgroundImage';
 
 const BackgroundImager: FC<PropsWithChildren> = ({ children }) => {
   const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  useLayoutEffect(() => {
+    getImage()
+  }, [selectedImage])
+
   const placeholderImageSource = require("@assets/sample_background.jpg");
 
   const pickImageAsync = async () => {
@@ -15,9 +21,32 @@ const BackgroundImager: FC<PropsWithChildren> = ({ children }) => {
     });
 
     if (!result.canceled) {
-      setSelectedImage(result.assets[0]?.uri);
+      const uri = result.assets[0]?.uri;
+      if(uri) {
+        setSelectedImage(uri);
+        storeImage(uri);
+      }
     } else {
       alert('You did not select any image.');
+    }
+  };
+
+  const storeImage = (uri: any) => {
+    try {
+      AsyncStorage.setItem('@backgroundImage', uri);
+    } catch (e) {
+      // saving error
+    }
+  };
+
+  const getImage = async () => {
+    try {
+      const uri = await AsyncStorage.getItem('@backgroundImage')
+      if(uri !== null) {
+        setSelectedImage(uri)
+      }
+    } catch(e) {
+      // error reading value
     }
   };
 
